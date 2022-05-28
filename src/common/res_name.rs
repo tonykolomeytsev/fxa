6831378463ea145399da579dd6b0c1ca49c0d6e5
Anton Kolomeytsev <akolomeytsev@vivid.money>
2022-05-29 01:01:45 +0300

    Add docs for http client and res_name

--- src/common/res_name.rs
+++ src/common/res_name.rs
@@ -1,3 +1,20 @@
+/// Cast the name to a valid form for use as a resource in an android app.
+/// The function turns any name into snake_case.
+///
+/// # Examples
+/// ```rust
+/// let source_name = "ImgAndroidBanner".to_string();
+/// let res_name = "img_android_banner".to_string();
+/// assert_eq!(to_res_name(&source_name), res_name);
+///
+/// let source_name = "ic_36/fingerprint".to_string();
+/// let res_name = "ic_36_fingerprint".to_string();
+/// assert_eq!(to_res_name(&source_name), res_name);
+///
+/// let source_name = "ic_24/paper_ID_leftAndroid 100%".to_string();
+/// let res_name = "ic_24_paper_id_left_android_100_".to_string();
+/// assert_eq!(to_res_name(&source_name), res_name);
+/// ```
 pub fn to_res_name(name: &String) -> String {
     let mut output = String::new();
     let mut i = 0u32;
