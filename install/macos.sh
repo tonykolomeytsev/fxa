a6287530689fd6a87a050c7028121f225b384f5d
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2022-06-06 00:42:54 +0300

    Update install script for macOS

--- install/macos.sh
+++ install/macos.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "Hello world"
+cleanup() {
+  printf "Cleaning up... "
+  rm -rf fxa-v0.1.0-test9-x86_64-apple-darwin.tar.gz
+  rm -rf dist
+  echo "Done"
+}
+
+trap 'cleanup' EXIT
+
+sudo curl -LJO https://github.com/tonykolomeytsev/fxa/releases/latest/download/fxa-v0.1.0-test9-x86_64-apple-darwin.tar.gz
+tar -xzvf fxa-v0.1.0-test9-x86_64-apple-darwin.tar.gz
+sudo mv ./dist/fxa /usr/local/bin/fxa
