3927746ca113eaaf1911c945b35d0e177eae6714
Anton Kolomeytsev <tonykolomeytsev@gmail.com>
2022-06-06 15:27:40 +0300

    Update mac os install script

--- install/macos.sh
+++ install/macos.sh
@@ -11,4 +11,5 @@ trap 'cleanup' EXIT
 
 sudo curl -LJO https://github.com/tonykolomeytsev/fxa/releases/latest/download/fxa-x86_64-apple-darwin.tar.gz
 tar -xzvf fxa-x86_64-apple-darwin.tar.gz
+sudo xattr -d com.apple.quarantine ./dist/fxa
 sudo mv ./dist/fxa /usr/local/bin/fxa
